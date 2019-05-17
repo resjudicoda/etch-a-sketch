73a45cd20ce6ffe0e32aa24c940486ac9eac5760
resjudicoda <adam.j.reis@gmail.com>
2019-05-17 14:52:41 -0500

    Completed etch-a-sketch

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,31 +1,62 @@
 
+for (let j = 0; j < 16; j++) {
+    const defaultGridColumn = document.createElement('div');
+    defaultGridColumn.classList.add('defaultGridColumn');
+    container.appendChild(defaultGridColumn);
+
+    for (let k = 0; k < 16; k++) {
+
+    const defaultGridSquare = document.createElement('div');
+    defaultGridSquare.classList.add('defaultGridSquare');
+    defaultGridColumn.appendChild(defaultGridSquare);
+    }
+}
+
+function random_rgba() {
+    let o = Math.round, r = Math.random, s = 255;
+    return 'rgba(' + o(r()*s) + ',' + o(r()*s) + ',' + o(r()*s) + ',' + r().toFixed(1) + ')';
+}
+
+let color = random_rgba();
+
+const divs = document.querySelectorAll( 'div.defaultGridSquare' );
+
+divs.forEach((div) => {
+
+div.addEventListener('mouseover', function(e){
+    e.target.style.background = 'darkgrey';
+})});
 
 let reset = document.querySelector('#Clear');
 
 reset.addEventListener('click', setGrid);
 
-function clearGrid () {
-
-    
-
-}
-
 function setGrid () {
 
     let gridNumber = prompt("Please set the size of the canvas.");
 
-    for (let i = 0; i < (gridNumber * gridNumber); i++) {
-        const gridSquare = document.createElement('div');
-        gridSquare.classList.add('gridSquare')
-        container.appendChild(gridSquare);
+    const clearDivs = document.querySelector('#container');
+    clearDivs.innerHTML = '';
+    
+    for (let i = 0; i < gridNumber; i++) {
+        const gridColumn = document.createElement('div');
+        gridColumn.classList.add('gridColumn');
+        container.appendChild(gridColumn);
+
+        for (let l = 0; l < gridNumber; l++) {
+
+            const gridSquare = document.createElement('div');
+            gridSquare.classList.add('gridSquare');
+            gridColumn.appendChild(gridSquare);
+            }
     }
 
-    const divs = document.querySelectorAll( 'div.gridSquare' );
+    const squares = document.querySelectorAll( 'div.gridSquare' );
 
-    divs.forEach((div) => {
+    squares.forEach((div) => {
 
     div.addEventListener('mouseover', function(e){
-        e.target.style.background = 'darkgrey'
+        e.target.style.background = 'darkgrey';
     })});
 
 }
